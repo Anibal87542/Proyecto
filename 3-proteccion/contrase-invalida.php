985921c29a85b1bf8c494d508cedcb5f53118d31
Anibal187542 <arcg89@hotmail.com>
2018-12-10 11:10:27 -0300

    456

---
+++ 3-proteccion/contrase-invalida.php
@@ -0,0 +1,348 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Contraseña Invalida</title>
+<meta name="generator" content="WYSIWYG Web Builder 14 - http://www.wysiwygwebbuilder.com">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+body
+{
+   background-color: #FFFFFF;
+   color: #000000;
+   font-family: Arial;
+   font-weight: normal;
+   font-size: 13px;
+   line-height: 1.1875;
+   margin: 0;
+   padding: 0;
+}
+a
+{
+   color: #0000FF;
+   text-decoration: underline;
+}
+a:visited
+{
+   color: #800080;
+}
+a:active
+{
+   color: #FF0000;
+}
+a:hover
+{
+   color: #0000FF;
+   text-decoration: underline;
+}
+input:focus, textarea:focus, select:focus
+{
+   outline: none;
+}
+#FlexGrid1
+{
+   display: grid;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #CCCCCC;
+   box-sizing: border-box;
+   margin: 0;
+   padding: 0;
+   font-size: 0px;
+   grid-template-columns: 1fr 2fr 1fr;
+   grid-template-rows: auto auto;
+   grid-template-areas:
+      "sidebar1 main sidebar2"
+      ". . .";
+}
+#FlexGrid1 .sidebar1
+{
+   display: flex;
+   grid-area: sidebar1;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #FFFFFF;
+   -webkit-flex-direction: column;
+   flex-direction: column;
+   -webkit-flex-wrap: nowrap;
+   flex-wrap: nowrap;
+   -webkit-justify-content: flex-start;
+   justify-content: flex-start;
+   -webkit-align-items: stretch;
+   align-items: stretch;
+   -webkit-align-content: stretch;
+   align-content: stretch;
+}
+#FlexGrid1 .main
+{
+   display: flex;
+   grid-area: main;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #FFFFFF;
+   padding: 20px 20px 20px 20px;
+   -webkit-flex-direction: column;
+   flex-direction: column;
+   -webkit-flex-wrap: nowrap;
+   flex-wrap: nowrap;
+   -webkit-justify-content: flex-start;
+   justify-content: flex-start;
+   -webkit-align-items: stretch;
+   align-items: stretch;
+   -webkit-align-content: stretch;
+   align-content: stretch;
+}
+#FlexGrid1 .sidebar2
+{
+   display: flex;
+   grid-area: sidebar2;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #FFFFFF;
+   -webkit-flex-direction: column;
+   flex-direction: column;
+   -webkit-flex-wrap: nowrap;
+   flex-wrap: nowrap;
+   -webkit-justify-content: flex-start;
+   justify-content: flex-start;
+   -webkit-align-items: stretch;
+   align-items: stretch;
+   -webkit-align-content: stretch;
+   align-content: stretch;
+}
+#footer
+{
+   display: -webkit-flex;
+   display: flex;
+   padding: 10px 10px 10px 10px;
+   -webkit-flex-direction: row;
+   flex-direction: row;
+   -webkit-flex-wrap: wrap;
+   flex-wrap: wrap;
+   -webkit-justify-content: space-between;
+   justify-content: space-between;
+   -webkit-align-items: center;
+   align-items: center;
+   -webkit-align-content: flex-start;
+   align-content: flex-start;
+   margin: 0;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #CCCCCC;
+   box-sizing: border-box;
+   font-size: 0px;
+}
+#wb_Image1
+{
+   margin: 0;
+   vertical-align: top;
+   -webkit-flex-grow: 0;
+   flex-grow: 0;
+   -webkit-flex-shrink: 0;
+   flex-shrink: 0;
+   -webkit-align-self: auto;
+   align-self: auto;
+}
+#Image1
+{
+   border: 0px solid #000000;
+   box-sizing: border-box;
+   padding: 0;
+   display:inline-block;
+   width: 100%;
+   height: auto;
+}
+#wb_Text1 
+{
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #000000;
+   -webkit-flex-grow: 1;
+   flex-grow: 1;
+   -webkit-flex-shrink: 1;
+   flex-shrink: 1;
+   -webkit-align-self: auto;
+   align-self: auto;
+   padding: 0;
+   margin: 13px 0px 0px 0px;
+   text-align: left;
+}
+#wb_Text1
+{
+   font-family: Arial;
+   font-weight: normal;
+   font-size: 13px;
+   line-height: 16px;
+}
+#wb_Text1 div
+{
+   text-align: left;
+   white-space: nowrap;
+}
+#wb_Image1
+{
+   display: inline-block;
+   z-index: 0;
+}
+@media only screen and (max-width: 1065px)
+{
+body
+{
+   background-color: #FFFFFF;
+   background-image: none;
+}
+#FlexGrid1
+{
+   visibility: visible;
+   display: grid;
+   font-size: 8px;
+   font-family: Arial;
+   font-weight: normal;
+   font-style: normal;
+   text-decoration: none;
+   background-color: transparent;
+   background-image: none;
+}
+#FlexGrid1
+{
+   margin: 0;
+   grid-column-gap: 0px;
+   grid-row-gap: 0px;
+   grid-template-columns: 1fr;
+   grid-template-rows: auto auto auto;
+   grid-template-areas:
+      "sidebar1"
+      "main"
+      "sidebar2";
+}
+#FlexGrid1 .sidebar1
+{
+   display: flex;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #FFFFFF;
+   -webkit-flex-direction: column;
+   flex-direction: column;
+   -webkit-flex-wrap: nowrap;
+   flex-wrap: nowrap;
+   -webkit-justify-content: space-around;
+   justify-content: space-around;
+   -webkit-align-items: stretch;
+   align-items: stretch;
+   -webkit-align-content: flex-start;
+   align-content: flex-start;
+}
+#FlexGrid1 .main
+{
+   display: flex;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #FFFFFF;
+   padding: 20px 20px 20px 20px;
+   -webkit-flex-direction: column;
+   flex-direction: column;
+   -webkit-flex-wrap: nowrap;
+   flex-wrap: nowrap;
+   -webkit-justify-content: flex-start;
+   justify-content: flex-start;
+   -webkit-align-items: stretch;
+   align-items: stretch;
+   -webkit-align-content: flex-start;
+   align-content: flex-start;
+}
+#FlexGrid1 .sidebar2
+{
+   display: flex;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #FFFFFF;
+   -webkit-flex-direction: column;
+   flex-direction: column;
+   -webkit-flex-wrap: nowrap;
+   flex-wrap: nowrap;
+   -webkit-justify-content: space-around;
+   justify-content: space-around;
+   -webkit-align-items: stretch;
+   align-items: stretch;
+   -webkit-align-content: flex-start;
+   align-content: flex-start;
+}
+#footer
+{
+   visibility: visible;
+   display: flex;
+   font-size: 8px;
+   font-family: Arial;
+   font-weight: normal;
+   font-style: normal;
+   text-decoration: none;
+   background-color: transparent;
+   background-image: none;
+}
+#footer
+{
+   margin: 0;
+   padding: 10px 10px 10px 10px;
+   -webkit-flex-direction: column;
+   flex-direction: column;
+   -webkit-flex-wrap: nowrap;
+   flex-wrap: nowrap;
+   -webkit-justify-content: space-between;
+   justify-content: space-between;
+   -webkit-align-items: stretch;
+   align-items: stretch;
+   -webkit-align-content: flex-start;
+   align-content: flex-start;
+}
+#wb_Text1
+{
+   width: auto;
+   height: auto;
+   visibility: visible;
+   display: inline;
+   font-size: 8px;
+   font-family: Arial;
+   font-weight: normal;
+   font-style: normal;
+   text-decoration: none;
+   background-color: transparent;
+   background-image: none;
+   -webkit-flex-grow: 1;
+   flex-grow: 1;
+   -webkit-flex-shrink: 1;
+   flex-shrink: 1;
+   -webkit-align-self: auto;
+   align-self: auto;
+}
+#wb_Text1
+{
+   font-family: Arial;
+   font-weight: normal;
+   font-size: 13px;
+   margin: 13px 0px 0px 0px;
+   padding: 0;
+   text-align: left;
+   line-height: 16px;
+}
+}
+</style>
+</head>
+<body>
+<div id="FlexGrid1">
+<div class="sidebar1">
+</div>
+<div class="main">
+<div id="wb_Image1">
+<img src="images/81.gif" id="Image1" alt="">
+</div>
+<div id="wb_Text1">
+<span style="color:#FF0000;">CONTRASEÑA O NOMBRE DE USUARIO INVALIDO, INTENTE&nbsp; DE NUEVO</span>
+</div>
+</div>
+<div class="sidebar2">
+</div>
+</div>
+<div id="footer">
+</div>
+</body>
+</html>
---
+++ 3-proteccion/registrnuevsocios.php
@@ -0,0 +1,398 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Registros para Nuevos Socios</title>
+<meta name="generator" content="WYSIWYG Web Builder 14 - http://www.wysiwygwebbuilder.com">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+body
+{
+   background-color: #FFFFFF;
+   color: #000000;
+   font-family: Arial;
+   font-weight: normal;
+   font-size: 13px;
+   line-height: 1.1875;
+   margin: 0;
+   padding: 0;
+}
+a
+{
+   color: #0000FF;
+   text-decoration: underline;
+}
+a:visited
+{
+   color: #800080;
+}
+a:active
+{
+   color: #FF0000;
+}
+a:hover
+{
+   color: #0000FF;
+   text-decoration: underline;
+}
+input:focus, textarea:focus, select:focus
+{
+   outline: none;
+}
+#FlexGrid1
+{
+   display: grid;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #CCCCCC;
+   box-sizing: border-box;
+   margin: 0;
+   padding: 0;
+   font-size: 0px;
+   grid-template-columns: 1fr 2fr 1fr;
+   grid-template-rows: auto auto;
+   grid-template-areas:
+      "sidebar1 main sidebar2"
+      ". . .";
+}
+#FlexGrid1 .sidebar1
+{
+   display: flex;
+   grid-area: sidebar1;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #FFFFFF;
+   -webkit-flex-direction: column;
+   flex-direction: column;
+   -webkit-flex-wrap: nowrap;
+   flex-wrap: nowrap;
+   -webkit-justify-content: flex-start;
+   justify-content: flex-start;
+   -webkit-align-items: stretch;
+   align-items: stretch;
+   -webkit-align-content: stretch;
+   align-content: stretch;
+}
+#FlexGrid1 .main
+{
+   display: flex;
+   grid-area: main;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #FFFFFF;
+   padding: 20px 20px 20px 20px;
+   -webkit-flex-direction: column;
+   flex-direction: column;
+   -webkit-flex-wrap: nowrap;
+   flex-wrap: nowrap;
+   -webkit-justify-content: flex-start;
+   justify-content: flex-start;
+   -webkit-align-items: stretch;
+   align-items: stretch;
+   -webkit-align-content: stretch;
+   align-content: stretch;
+}
+#FlexGrid1 .sidebar2
+{
+   display: flex;
+   grid-area: sidebar2;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #FFFFFF;
+   -webkit-flex-direction: column;
+   flex-direction: column;
+   -webkit-flex-wrap: nowrap;
+   flex-wrap: nowrap;
+   -webkit-justify-content: flex-start;
+   justify-content: flex-start;
+   -webkit-align-items: stretch;
+   align-items: stretch;
+   -webkit-align-content: stretch;
+   align-content: stretch;
+}
+#wb_Heading1
+{
+   background-color: #006400;
+   background-image: none;
+   border: 0px solid #000000;
+   border-radius: 4px;
+   margin: 0px 0px 20px 0px;
+   padding: 16px 16px 16px 16px;
+   text-align: center;
+   -webkit-flex-grow: 0;
+   flex-grow: 0;
+   -webkit-flex-shrink: 0;
+   flex-shrink: 0;
+   -webkit-align-self: auto;
+   align-self: auto;
+}
+#Heading1
+{
+   color: #000000;
+   font-family: Arial;
+   font-weight: normal;
+   font-size: 21px;
+   margin: 0;
+   text-align: center;
+}
+#footer
+{
+   display: -webkit-flex;
+   display: flex;
+   padding: 10px 10px 10px 10px;
+   -webkit-flex-direction: row;
+   flex-direction: row;
+   -webkit-flex-wrap: wrap;
+   flex-wrap: wrap;
+   -webkit-justify-content: space-between;
+   justify-content: space-between;
+   -webkit-align-items: center;
+   align-items: center;
+   -webkit-align-content: flex-start;
+   align-content: flex-start;
+   margin: 0;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #CCCCCC;
+   box-sizing: border-box;
+   font-size: 0px;
+}
+#wb_Text1 
+{
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #000000;
+   -webkit-flex-grow: 1;
+   flex-grow: 1;
+   -webkit-flex-shrink: 1;
+   flex-shrink: 1;
+   -webkit-align-self: auto;
+   align-self: auto;
+   padding: 0;
+   margin: 11px 0px 0px 6px;
+   text-align: justify;
+}
+#wb_Text1
+{
+   font-family: Arial;
+   font-weight: normal;
+   font-size: 13px;
+   line-height: 16px;
+}
+#wb_Text1 div
+{
+   text-align: justify;
+   white-space: normal;
+}
+#wb_Heading1
+{
+   display: inline-block;
+   z-index: 0;
+}
+@media only screen and (max-width: 1065px)
+{
+body
+{
+   background-color: #FFFFFF;
+   background-image: none;
+}
+#FlexGrid1
+{
+   visibility: visible;
+   display: grid;
+   font-size: 8px;
+   font-family: Arial;
+   font-weight: normal;
+   font-style: normal;
+   text-decoration: none;
+   background-color: transparent;
+   background-image: none;
+}
+#FlexGrid1
+{
+   margin: 0;
+   grid-column-gap: 0px;
+   grid-row-gap: 0px;
+   grid-template-columns: 1fr;
+   grid-template-rows: auto auto auto;
+   grid-template-areas:
+      "sidebar1"
+      "main"
+      "sidebar2";
+}
+#FlexGrid1 .sidebar1
+{
+   display: flex;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #FFFFFF;
+   -webkit-flex-direction: column;
+   flex-direction: column;
+   -webkit-flex-wrap: nowrap;
+   flex-wrap: nowrap;
+   -webkit-justify-content: space-around;
+   justify-content: space-around;
+   -webkit-align-items: stretch;
+   align-items: stretch;
+   -webkit-align-content: flex-start;
+   align-content: flex-start;
+}
+#FlexGrid1 .main
+{
+   display: flex;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #FFFFFF;
+   padding: 20px 20px 20px 20px;
+   -webkit-flex-direction: column;
+   flex-direction: column;
+   -webkit-flex-wrap: nowrap;
+   flex-wrap: nowrap;
+   -webkit-justify-content: flex-start;
+   justify-content: flex-start;
+   -webkit-align-items: stretch;
+   align-items: stretch;
+   -webkit-align-content: flex-start;
+   align-content: flex-start;
+}
+#FlexGrid1 .sidebar2
+{
+   display: flex;
+   background-color: transparent;
+   background-image: none;
+   border: 0px solid #FFFFFF;
+   -webkit-flex-direction: column;
+   flex-direction: column;
+   -webkit-flex-wrap: nowrap;
+   flex-wrap: nowrap;
+   -webkit-justify-content: space-around;
+   justify-content: space-around;
+   -webkit-align-items: stretch;
+   align-items: stretch;
+   -webkit-align-content: flex-start;
+   align-content: flex-start;
+}
+#wb_Heading1
+{
+   width: auto;
+   height: auto;
+   visibility: visible;
+   display: inline;
+   color: #000000;
+   font-size: 21px;
+   font-family: Arial;
+   font-weight: normal;
+   font-style: normal;
+   text-decoration: none;
+   background-color: #006400;
+   background-image: none;
+   border-radius: 4px;
+   margin: 0px 0px 20px 0px;
+   padding: 16px 16px 16px 16px;
+   -webkit-flex-grow: 0;
+   flex-grow: 0;
+   -webkit-flex-shrink: 0;
+   flex-shrink: 0;
+   -webkit-align-self: auto;
+   align-self: auto;
+}
+#wb_Heading1
+{
+   background-color: #006400;
+   background-image: none;
+   border: 0px solid #000000;
+   border-radius: 4px;
+   text-align: center;
+   -webkit-flex-grow: 0;
+   flex-grow: 0;
+   -webkit-flex-shrink: 0;
+   flex-shrink: 0;
+   -webkit-align-self: auto;
+   align-self: auto;
+}
+#Heading1
+{
+   color: #000000;
+   font-family: Arial;
+   font-weight: normal;
+   font-size: 21px;
+}
+#footer
+{
+   visibility: visible;
+   display: flex;
+   font-size: 8px;
+   font-family: Arial;
+   font-weight: normal;
+   font-style: normal;
+   text-decoration: none;
+   background-color: transparent;
+   background-image: none;
+}
+#footer
+{
+   margin: 0;
+   padding: 10px 10px 10px 10px;
+   -webkit-flex-direction: column;
+   flex-direction: column;
+   -webkit-flex-wrap: nowrap;
+   flex-wrap: nowrap;
+   -webkit-justify-content: space-between;
+   justify-content: space-between;
+   -webkit-align-items: stretch;
+   align-items: stretch;
+   -webkit-align-content: flex-start;
+   align-content: flex-start;
+}
+#wb_Text1
+{
+   width: auto;
+   height: auto;
+   visibility: visible;
+   display: inline;
+   font-size: 8px;
+   font-family: Arial;
+   font-weight: normal;
+   font-style: normal;
+   text-decoration: none;
+   background-color: transparent;
+   background-image: none;
+   -webkit-flex-grow: 1;
+   flex-grow: 1;
+   -webkit-flex-shrink: 1;
+   flex-shrink: 1;
+   -webkit-align-self: auto;
+   align-self: auto;
+}
+#wb_Text1
+{
+   font-family: Arial;
+   font-weight: normal;
+   font-size: 13px;
+   margin: 4px 0px 0px 0px;
+   padding: 0;
+   text-align: justify;
+   line-height: 16px;
+}
+}
+</style>
+</head>
+<body>
+<div id="FlexGrid1">
+<div class="sidebar1">
+</div>
+<div class="main">
+<div id="wb_Heading1">
+<h1 id="Heading1">REGISTROS PARA NUEVOS SOCIOS</h1>
+</div>
+<div id="wb_Text1">
+<span style="color:#000000;">Para poder tener acceso al interior del sistema se debe formar parte de la <strong>ASOCIACION DE ELECTRICISTA DE PRESIDENTE FRANCO - ASEFRAN.</strong> En caso de que ya formes parte y solo necesitas tu usuario y contraseña podes enviar un correo al administrador de la pagina para que este ingrese tus datos al sistema.<br><br>Podes enviar tus datos al correo de la asociacion <strong>asefran@outlook.com</strong> favor llenar con tu <strong>nombre completo telefono y correo</strong>.<br><br><br><a href="./../index.html">Pagina de Inicio</a></span>
+</div>
+</div>
+<div class="sidebar2">
+</div>
+</div>
+<div id="footer">
+</div>
+</body>
+</html>
